14651bac229917fa964246eed22e00d54cf925af
callum-code-dev-tech <callumwithington2006@gmail.com>
2026-01-18 23:14:02 +0000

    added pictures

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const picturesContainer = document.querySelector(`.pictures`)
 const bigShow = document.getElementById(`bigShow`)
 
-const img = 
+function createPictures(){
+    images.forEach(function(image){
+        const imageElement = document.createElement(`img`)
+        imageElement.src = image.src
+        imageElement.alt = image.alt
+        
+        picturesContainer.append(imageElement)
+
+        imageElement.addEventListener(`click`)
+        }
+    }
+}
