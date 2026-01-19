487716a85a0bf69e061d3aa7336e6f88afe2c95e
callum-code-dev-tech <callumwithington2006@gmail.com>
2026-01-19 14:45:41 +0000

    added buttons

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const picturesContainer = document.querySelector(`.pictures`)
-const bigShow = document.getElementById(`bigShow`)
+const photoContainer = document.querySelector("photo");
+const bigShow = document.getElementById(`bigShow`);
+const left = document.getElementById("leftBtn");
 
-function createPictures(){
-    images.forEach(function(image){
-        const imageElement = document.createElement(`img`)
-        imageElement.src = image.src
-        imageElement.alt = image.alt
-        
-        picturesContainer.append(imageElement)
+let currentIndex = 0;
+let isShown = true;
 
+const image = [
+    {
+        src: "https://images.unsplash.com/photo-1587654780291-39c9404d746b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGVnb3xlbnwwfHwwfHx8MA%3D%3D",
+        alt: "Group",
+    },
+    {
+        src: "https://images.unsplash.com/photo-1633469924738-52101af51d87?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGVnb3xlbnwwfHwwfHx8MA%3D%3D",
+        alt: "Hill",
+    },
+    {
+        src: "https://images.unsplash.com/photo-1585366119957-e9730b6d0f60?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGVnb3xlbnwwfHwwfHx8MA%3D%3D",
+        alt: "Band",
+    },
+    {
+        src: "https://images.unsplash.com/photo-1611604548018-d56bbd85d681?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bGVnb3xlbnwwfHwwfHx8MA%3D%3D",
+        alt: "Superbat",
+    },
+    {
+        src: "https://images.unsplash.com/photo-1505322033502-1f4385692e6a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxlZ298ZW58MHx8MHx8fDA%3D",
+        alt: "Vilage",
+    },
+];
+
+function createPics(){
+    images.forEach((image) => {
+        const imageElem = document.createElement(`img`)
+        imageElem.src = image.src
+        imageElem.alt = image.alt
+        imageElem.className = "pictures";
         imageElement.addEventListener(`click`)
-        }
+        })
     }
-}
